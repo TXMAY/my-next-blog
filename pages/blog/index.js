4df4c59289f8a8aab1298e55ce32f2dfa8b62b4f
TXMAY <47449096+TXMAY@users.noreply.github.com>
2022-07-13 11:43:16 +0900

    init

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 import axious from "axios";
 import { useEffect, useState } from "react";
 import styles from "./index.module.css";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const [blogList, setBlogList] = useState([]);
+  const router = useRouter();
 
   const fetchData = async () => {
     const result = await axious.get("/api/blog");
@@ -16,12 +18,22 @@ export default function Home() {
     fetchData();
   }, []);
 
+  const clickBlog = (id) => {
+    return () => {
+      router.push(`/blog/${id}`);
+    };
+  };
+
   return (
     <div>
       블로그 목록
       {blogList.map((blog) => {
         return (
-          <div className={styles.blog} key={blog.id}>
+          <div
+            className={styles.blog}
+            key={blog.id}
+            onClick={clickBlog(blog.id)}
+          >
             <div>{blog.title}</div>
           </div>
         );
